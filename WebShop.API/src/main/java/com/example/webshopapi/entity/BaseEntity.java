9429edf49297b1751883015d8bf9b4f9c5b1424b
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-09-20 10:02:15 +0300

    Add @Data to BaseEntity class to access the Id property

--- WebShop.API/src/main/java/com/example/webshopapi/entity/BaseEntity.java
+++ WebShop.API/src/main/java/com/example/webshopapi/entity/BaseEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
