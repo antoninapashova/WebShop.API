f39254be963d915bf12cf56dbbcbddba1fe71fcc
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-09-20 10:27:05 +0300

    Add custom UserDetails service to load user data

---
+++ WebShop.API/src/main/java/com/example/webshopapi/service/jwt/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.webshopapi.service.jwt;
+
+import com.example.webshopapi.entity.UserEntity;
+import com.example.webshopapi.entity.UserPrinciple;
+import com.example.webshopapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> user = userRepository.findByUsername(username);
+        if (user.isEmpty()) throw new UsernameNotFoundException("User 404");
+
+        return new UserPrinciple(user.get());
+    }
+}
