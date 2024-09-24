bcf9a170cbd0bf47d11c4ddf5aa52eb1e36dd9ca
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-09-24 14:02:39 +0300

    Fix bug when check user authority

--- WebShop.API/src/main/java/com/example/webshopapi/entity/UserPrinciple.java
+++ WebShop.API/src/main/java/com/example/webshopapi/entity/UserPrinciple.java
@@ -16,7 +16,7 @@ public class UserPrinciple implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
     }
 
     @Override
