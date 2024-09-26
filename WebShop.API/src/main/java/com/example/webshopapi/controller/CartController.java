f55c3d13a0849d5b6bafcb9ea139609408ca89ce
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-09-26 17:52:42 +0300

    Setup endpoints for change item quantity and set item quantity

--- WebShop.API/src/main/java/com/example/webshopapi/controller/CartController.java
+++ WebShop.API/src/main/java/com/example/webshopapi/controller/CartController.java
@@ -1,16 +1,15 @@
 package com.example.webshopapi.controller;
 
 import com.example.webshopapi.dto.CartDto;
+import com.example.webshopapi.dto.requestObjects.ChangeCartItemQuantityRequest;
+import com.example.webshopapi.dto.requestObjects.SetCartItemQuantityRequest;
 import com.example.webshopapi.entity.UserPrinciple;
 import com.example.webshopapi.service.CartService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -29,13 +28,29 @@ public class CartController {
     }
 
     @GetMapping("/get-cart")
-    public ResponseEntity<CartDto> getCart(@AuthenticationPrincipal UserPrinciple user) throws Exception {
+    public ResponseEntity<CartDto> getCart(@AuthenticationPrincipal UserPrinciple user) {
         CartDto cart = cartService.getCartByUserId(user.getUserId());
 
-        if(cart==null){
+        if (cart == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(cart);
+    }
+
+    @PutMapping("/cart/changeItemQuantity")
+    public ResponseEntity<?> increaseCartItemQuantity(@RequestBody ChangeCartItemQuantityRequest changeQuantity)  {
+         if(changeQuantity == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+         cartService.changeItemQuantity(changeQuantity);
+         return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @PutMapping("/cart/setItemQuantity")
+    public ResponseEntity<?> setCartItemQuantity(@RequestBody SetCartItemQuantityRequest setQuantity)  {
+        if(setQuantity == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+        cartService.setCartItemQuantity(setQuantity.cartItemId, setQuantity.quantity);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 }
