8b9743b8202a86dbcde7c9e758c4d9234f3cb1fc
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-01 15:18:14 +0300

    Setup endpoint for retrieving orders from database and secure to be available only for admins

--- WebShop.API/src/main/java/com/example/webshopapi/controller/OrderController.java
+++ WebShop.API/src/main/java/com/example/webshopapi/controller/OrderController.java
@@ -1,12 +1,17 @@
 package com.example.webshopapi.controller;
 
+import com.example.webshopapi.dto.OrderDto;
 import com.example.webshopapi.entity.UserPrinciple;
 import com.example.webshopapi.service.OrderService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -18,5 +23,11 @@ public class OrderController {
     {
         orderService.createOrder(principle.getUserId());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/all-orders")
+    public ResponseEntity<List<OrderDto>> retrieveAllOrders(){
+        List<OrderDto> orderDtoList = orderService.retrieveAllOrders();
+        return ResponseEntity.status(HttpStatus.OK).body(orderDtoList);
     }
 }
