22152a5a6089a6c978eae9ff337e906a6f375909
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-11 14:58:26 +0300

    Add properties to ProductDto

--- WebShop.API/src/main/java/com/example/webshopapi/dto/ProductDto.java
+++ WebShop.API/src/main/java/com/example/webshopapi/dto/ProductDto.java
@@ -4,7 +4,10 @@ import lombok.Data;
 
 @Data
 public class ProductDto {
+    public String id;
     public String name;
     public int quantity;
     public double price;
+    public String description;
+    public String categoryName;
 }
