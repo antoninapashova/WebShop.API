86e7989922324ec28974e87c4aefeee5f76ddba5
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-11 10:28:27 +0300

    Remove old cors configuration bean

--- WebShop.API/src/main/java/com/example/webshopapi/config/ApplicationBeanConfiguration.java
+++ WebShop.API/src/main/java/com/example/webshopapi/config/ApplicationBeanConfiguration.java
@@ -3,23 +3,11 @@ package com.example.webshopapi.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationBeanConfiguration {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("http://localhost:57752");
-            }
-        };
     }
 }
