a4a580b674fdfb9cdf352457c5349ac3e7f5c154
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-14 14:49:24 +0300

    Add Id property in CartItemDto

--- WebShop.API/src/main/java/com/example/webshopapi/dto/CartItemDto.java
+++ WebShop.API/src/main/java/com/example/webshopapi/dto/CartItemDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class CartItemDto {
+    private String id;
     private String name;
     private int quantity;
     private double price;
