db9a3b72a6c8bb0390a5e7513a3d91385b9e6de2
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-24 10:14:36 +0300

    Use native query in repository when fetching products by name

--- WebShop.API/src/main/java/com/example/webshopapi/repository/ProductRepository.java
+++ WebShop.API/src/main/java/com/example/webshopapi/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.example.webshopapi.repository;
 
 import com.example.webshopapi.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,6 @@ import java.util.UUID;
 public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
     boolean existsByName(String name);
     ProductEntity findProductEntityById(UUID id);
+    @Query(value = "SELECT * FROM products WHERE name = ?1", nativeQuery = true)
     List<ProductEntity> findAllByName(String name);
 }
