7a832720af8066c0a98e99214111d9526af1eb9b
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-30 17:24:20 +0200

    Change property name

--- WebShop.API/src/main/java/com/example/webshopapi/dto/requestObjects/SignupRequest.java
+++ WebShop.API/src/main/java/com/example/webshopapi/dto/requestObjects/SignupRequest.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class SignupRequest {
     private String email;
     private String password;
-    private String name;
+    private String username;
     private String firstName;
     private String lastName;
 }
