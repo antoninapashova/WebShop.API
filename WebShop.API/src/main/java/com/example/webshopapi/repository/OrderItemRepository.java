49d054359b4275d44e56b76db1195998378048fb
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-12-05 13:42:12 +0200

    Rewrite sql query for fetching order items

--- WebShop.API/src/main/java/com/example/webshopapi/repository/OrderItemRepository.java
+++ WebShop.API/src/main/java/com/example/webshopapi/repository/OrderItemRepository.java
@@ -3,6 +3,7 @@ package com.example.webshopapi.repository;
 import com.example.webshopapi.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,6 @@ import java.util.UUID;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
-    @Query(value = "SELECT * FROM order_items WHERE order_id = ?1", nativeQuery = true)
-    List<OrderItem> findAllByOrderId(UUID orderId);
+    @Query(value = "SELECT * FROM order_items WHERE order_id = :orderId", nativeQuery = true)
+    List<OrderItem> findAllByOrderId(@Param("orderId") UUID orderId);
 }
